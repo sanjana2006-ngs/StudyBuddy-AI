9fa6a74ec9e154367f5d4bb99ea223a4317ae114
sanjana2006-ngs <gayathrisanjananukala@gmail.com>
2025-12-23 23:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from openai import OpenAI
 
 client = OpenAI(api_key="YOUR_API_KEY")
 
-if "OPENAI_API_KEY" not in st.secrets:
-    st.error("❌ OpenAI API key not found in Streamlit Secrets")
-    st.stop()
-
-
 st.set_page_config(page_title="StudyBuddy AI", page_icon="🤖")
 
 if "logged_in" not in st.session_state:
